18a14c7478be1df82afbae3e1e4f55c36ab6c78b
Gabi Dobocan <gabi@telepat.io>
2022-09-09 18:33:48 +0300

    Replace DynamoDB with Mongo

--- handler.js
+++ handler.js
@@ -1,27 +1,31 @@
 'use strict';
 
-const {v4: uuidv4} = require('uuid');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const AWS = require('aws-sdk');
+
 AWS.config.update({ region: 'us-east-1' });
-const ddb = new AWS.DynamoDB();
+
+const clientPromise = new MongoClient(
+  `mongodb+srv://${process.env.AWS_ACCESS_KEY_ID}:${encodeURIComponent(
+    process.env.AWS_SECRET_ACCESS_KEY
+  )}@sandwormdb.pafv8.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:${encodeURIComponent(
+    process.env.AWS_SESSION_TOKEN
+  )}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  }
+).connect();
 
 module.exports.ingest = async (event) => {
   try {
-    await ddb
-      .batchWriteItem({
-        RequestItems: {
-          CollectedPermissions: JSON.parse(event.body).map((item) => ({
-            PutRequest: {
-              Item: AWS.DynamoDB.Converter.marshall({
-                ...item,
-                uuid: uuidv4(),
-                timestamp: Date.now(),
-              }),
-            },
-          })),
-        },
-      })
-      .promise();
+    const client = await clientPromise;
+    const collection = client.db('permissions').collection('raw');
+
+    await collection.insertMany(
+      JSON.parse(event.body).map((item) => ({ ...item, timestamp: Date.now() }))
+    );
 
     return {
       statusCode: 200,
